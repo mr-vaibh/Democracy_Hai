83b8ddada3571594a3ef22a2e9451f4a5c1f4e74
Vaibhav Shukla <shuklavaibhav336@gmail.com>
2021-02-09 23:10:17 +0530

    added '/' path (home.urls), and static url too

--- democracyhai/urls.py
+++ democracyhai/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+# Serving static files in Developlment
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', include('home.urls', namespace="home")),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
