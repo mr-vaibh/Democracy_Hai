1806c3f428a1c61d355eb86da8222d3b36a2649a
Vaibhav Shukla <shuklavaibhav336@gmail.com>
2021-02-11 00:43:35 +0530

    Reports model CREATED

--- home/models.py
+++ home/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Reports():
+    name = models.CharField(max_length=255)
+    gender = models.ChoiceField(choices=["Male", "Female", "Transgender", "Others (If a group/organisation)"])
+    country = models.CharField(max_length=50)
+    address = models.CharField(max_length=500)
+    state = models.CharField(max_length=50)
+    district = models.CharField(max_length=50)
+    pincode = models.IntegerField()
+    email = models.EmailField(max_length=255)
+    phoneno = models.PhoneNumberField()
+    category = models.ChoiceField(choices=["Public Grievances", "Suggestion/Feedback", "Financial Fraud/Scam", "Greetings/Wishes", "Appointment with PM", "Message Requests"])
+    query = models.CharField(max_length=5000)
+    identity_no = models.CharField(max_length=100)
+    identity_doc = models.FileField(upload_to='/media/reports/identities')
+    user_upload = models.FileField(upload_to='/media/reports/useruploads')
+    reviewed = models.BooleanField(default=False)
+    fulfilled = models.BooleanField(default=False)
