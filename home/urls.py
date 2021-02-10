fdbd3921a255ee29baf6ba827e9d9c70262bdfaf
Vaibhav Shukla <shuklavaibhav336@gmail.com>
2021-02-10 15:28:46 +0530

    added STATIC_URL

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
+# Serving static files in Developlment
+from django.conf import settings
+from django.conf.urls.static import static
+
 from . import views
 
 app_name = 'home'
 
 urlpatterns = [
 	path('', views.index, name='index'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
